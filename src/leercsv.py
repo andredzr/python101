0fd68cc40b1025837b6f482f5139a7b193bc7d41
David André Díaz <david.diaz@enroutesystems.com>
2020-11-27 12:56:37 -0600

    Primer commit del proyecto, se va el .DS_Storegit status

---
+++ src/leercsv.py
@@ -0,0 +1,16 @@
+import csv
+
+def leer_csv():
+
+    path_archivo = 'data/empleados.csv'
+
+    with open(path_archivo) as csv_file:
+        reader = csv.reader(csv_file)
+        for linea in reader:
+            print(linea)
+
+
+
+
+if __name__ == '__main__':
+    leer_csv()
---
+++ src/webservice.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/json', methods=['GET'])
+def ejemplo_json():
+    contenido={"id" : 1, "nombre" : "Juan", "apellido" : "algun apellido"}
+    segundo = {"id": 2, "nombre": "Javier", "apellido": "Zepeda"}
+    lista = [contenido, segundo]
+    return jsonify(lista)
+
+@app.route('/', methods=['GET'])
+def hola_mundo_con_flask():
+    return 'Ave, munde'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
