ea4347f667e600d2c73151edb1f0528abd58389b
David André Díaz <david.diaz@enroutesystems.com>
2020-11-27 15:40:41 -0600

    dejar el webservice de transformación csv->json prácticamente listo para hacer pull en linode

--- src/webservice.py
+++ src/webservice.py
@@ -1,18 +1,16 @@
 from flask import Flask, jsonify
+import csv
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def ejemplo_json():
-    contenido={"id" : 1, "nombre" : "Juan", "apellido" : "algun apellido"}
-    segundo = {"id": 2, "nombre": "Javier", "apellido": "Zepeda"}
-    lista = [contenido, segundo]
-    return jsonify(lista)
-
-#@app.route('/', methods=['GET'])
-#def hola_mundo_con_flask():
- #   return 'Ave, munde'
-
+def csv_a_json():
+    with open('data/employees.csv', 'r') as archivo_csv:
+        lector_dict = csv.DictReader(archivo_csv)
+        lista = []
+        for linea in lector_dict:
+            lista.append(linea)
+        return jsonify(lista)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
